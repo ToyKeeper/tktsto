ae3bc4d5743247a5a8cad06d612bbe5e57fb1417
Selene ToyKeeper <git@toykeeper.net>
2026-01-20 09:45:09 -0700

    api.js: detect Edge, Brave, and Maxthon and log which one(s) were detected

--- api.js
+++ api.js
@@ -6,8 +6,23 @@
 
 // browser (Firefox) vs chrome (Chromium)
 export const isFirefox = (typeof browser !== 'undefined');
+// if not Firefox, it's some flavor of Chromium
 export const isChrome = (! isFirefox);
-//export const isEdge = ???;
+// MS Edge
+export let isEdge = / Edg\//.test(navigator.userAgent);
+if (navigator.userAgentData?.brands) {
+  isEdge = navigator.userAgentData.brands
+    .some(b => b.brand === "Microsoft Edge");
+}
+// Brave
+export const isBrave = (typeof navigator.brave !== 'undefined');
+// Vivaldi ... is hard to detect
 //export const isVivaldi = ???;
+// Maxthon
+export const isMaxthon = (typeof maxthon !== 'undefined');
+
+// select a base symbol for all browser API calls
 export const api = isFirefox ? browser : chrome;
+
+console.log(`Browser type: Firefox(${isFirefox}) Chrome(${isChrome}) Edge(${isEdge}) Brave(${isBrave}) Vivaldi(???) Maxthon(${isMaxthon})`);
 
